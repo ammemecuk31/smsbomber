925d8e00a917e3346d349773592f7523c5347168
Kıbrıslı <muhammedyesilkaya4262@gmail.com>
2026-03-16 20:32:19 +0300

    smsbomber.py

--- smsbomber.py
+++ smsbomber.py
@@ -28,7 +28,7 @@ while 1:
     system("cls||clear")
     print("""{}
     
-    Sms: {}           {}by {}@Codza\n  
+    Sms: {}           {}by {}@Kıbrıslı\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
